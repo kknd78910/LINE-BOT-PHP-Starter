198b860f92d38cea3837ad748ea6c2c8813c5494
kknd78910 <wachi22@gmail.com>
2017-01-18 01:28:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,9 +18,20 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
+			{
 				'type' => 'text',
 				'text' => $text
+			},
+			{
+				'type' => 'text',
+				'text' => ทำไร
+			}
+
+
+
 			];
+
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
