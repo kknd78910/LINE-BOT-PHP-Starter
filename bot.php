b2c377debc666f7e2aeee5f75e6620e40e99e01d
kknd78910 <wachi22@gmail.com>
2017-01-19 00:15:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -30,42 +30,6 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [เรางง],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	else	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			
-			// Get text sent
-			$text = $event['message']['text'];
-			
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => ทำงาน
-				
-			];
-			
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
 				'messages' => [ทำไร],
 			];
 			$post = json_encode($data);
@@ -82,6 +46,7 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
+	
 	}
 }
 echo "OK5";
