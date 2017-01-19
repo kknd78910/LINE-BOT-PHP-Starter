cab54c512027ca57681fea019c331aaf7d84adec
kknd78910 <wachi22@gmail.com>
2017-01-20 01:39:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,8 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = ['type' => 'text','text' => $text]
+			$messages = ['type' => 'text',
+				     'text' => hello]
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -47,4 +48,4 @@ if (!is_null($events['events'])) {
 	
 	}
 }
-echo "OK8";
+echo "OK9";
