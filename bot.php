4388db44e2884b1cfef74cb57b9fa1c80f337efa
kknd78910 <wachi22@gmail.com>
2017-01-20 01:44:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['test']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [test],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
