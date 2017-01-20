17331bdd7beb74945d72f5685a2f148fbb61f0e0
kknd78910 <wachi22@gmail.com>
2017-01-20 15:04:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' =>hello
+				'text' =>[$text][$text]
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -45,4 +45,4 @@ if (!is_null($events['events'])) {
 	}
 }
 	echo $result . "\r\n";
-echo "OK1";
+echo "OK";
