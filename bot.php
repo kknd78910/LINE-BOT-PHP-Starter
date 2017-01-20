07bfa51ffb6dc1105f196b55f5d710473a93e0e7
kknd78910 <wachi22@gmail.com>
2017-01-20 17:00:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,24 +18,17 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			/*$messages = [
+			$messages = [
+				{
 				'type' => 'text',
 				'text' => $text
-			];*/
+				},
+				{
+				'type' => 'text',
+				'text' => $text
+				}
+			];
 			
-		 "messages":[
-        {
-            "type":"text",
-            "text":"Hello, world1"
-        },
-        {
-            "type":"text",
-            "text":"Hello, world2"
-        }
-    ]
-		
-		
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -58,5 +51,5 @@ if (!is_null($events['events'])) {
 		//}
 	}
 }
-echo "OK";
+echo "OK2";
 echo '$text';
